d155d10f9845428b409d91cab68c4cae18f407b8
shikharkamal123 <shikharkamal123@gmail.com>
2020-04-19 13:30:57 +0530

    second commit

---
+++ download.php
@@ -0,0 +1,21 @@
+
+<?php
+
+$file  = $_GET['file'].".pdf" ;
+header("content-disposition: attachment; filename=" .urlencode($file));
+
+$fb = fopen($file, "r");
+
+while(!feof($fb)){
+echo fread($fb, 8192);
+flush();
+}
+
+fclose($fb);
+
+
+
+?>
+
+<!--fread(length): " The function will stop at the end of the file or when it reaches the specified length,
+ whichever comes first."-->
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+<title>Download pdf using php from html</title>
+
+</head>
+
+<body>
+
+<center>
+
+<h1 style="color:#000;">Welcome to the world</h1>
+<p><strong>click below to download pdf</p></strong>
+<a href="sample.pdf" target="_blank">DOWNLOAD PDF </a>
+</center>
+
+
+
+</body>
+
+</html>
